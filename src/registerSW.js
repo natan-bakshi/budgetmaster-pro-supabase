3be3c24dc2bacbf72549d43b7d8b9b91ff82086c
natan-bakshi <natib8000@gmail.com>
2026-02-24 21:24:23 +0200

    Add PWA manifest and service worker

---
+++ src/registerSW.js
@@ -0,0 +1,10 @@
+export function registerServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((reg) => console.log('SW registered:', reg.scope))
+        .catch((err) => console.error('SW registration failed:', err));
+    });
+  }
+}
